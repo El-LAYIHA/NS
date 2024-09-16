b78ce29b0c3bab2ed7b24ef44fca3793bfef5b7f
Muhammed ALBAYRAK <muhammed.samil.albayrak@gmail.com>
2024-09-17 00:57:50 +0300

    Bir kaç düzenleme.

--- ns-HM_Ashada/Alem/Eshas/Meziyet/Yayi.cs
+++ ns-HM_Ashada/Alem/Eshas/Meziyet/Yayi.cs
@@ -1,13 +1,10 @@
-﻿using ns_HM_Ashada.Alem.Asar;
-using ns_HM_Ashada.Alem.Emkine;
-
-namespace ns_HM_Ashada.Alem.Eshas.Meziyet
+﻿namespace ns_HM_Ashada.Alem.Eshas.Meziyet
 {
     public interface Yayi
     {
-        public void Yayin(Katip Kim, Bina Nerede, Metn Neyi);
+        public void Yayin();
         
-        public void Yazdir(Bina Nerede, Metn Neyi);
-        public void Sundur(Bina Nerede);
+        public void Yazdir(Katip Kim);
+        public void Sundur(Sunucu Kim);
     }
 }
